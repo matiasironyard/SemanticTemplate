618839ea76f2581a04abc8f83ad763a60d984952
Matias Mariani <hi@matiasmariani.io>
2017-12-19 14:06:00 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+
+
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+
+		};
+	}
+
+	render() {
+		
+		return (
+          <div>hi</div>
+			
+		);
+	}
+}
+
+render(<App />, document.getElementById("root"));
